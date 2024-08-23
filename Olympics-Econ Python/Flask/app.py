1991892295b2512fcdac5c7d384f313f014d2ffc
MacaroniMacks <122694751+MacaroniMacks@users.noreply.github.com>
2024-08-22 22:16:43 -0700

    small change
    Small change in app.py so it can run on render

--- Olympics-Econ Python/Flask/app.py
+++ Olympics-Econ Python/Flask/app.py
@@ -18,4 +18,5 @@ def results():
     return generateGraphs(country, sport)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+
